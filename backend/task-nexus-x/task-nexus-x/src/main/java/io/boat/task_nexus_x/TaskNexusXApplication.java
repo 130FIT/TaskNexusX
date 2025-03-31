2e648c926cfea3dd220c57999d76d224c5182717
BNGSupakit <supakit_b@brainergy.digital>
2025-04-01 01:09:29 +0700

    [Backend] add init task nexus x Backend

---
+++ backend/task-nexus-x/task-nexus-x/src/main/java/io/boat/task_nexus_x/TaskNexusXApplication.java
@@ -0,0 +1,13 @@
+package io.boat.task_nexus_x;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskNexusXApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskNexusXApplication.class, args);
+	}
+
+}
---
+++ backend/task-nexus-x/task-nexus-x/src/test/java/io/boat/task_nexus_x/TaskNexusXApplicationTests.java
@@ -0,0 +1,13 @@
+package io.boat.task_nexus_x;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskNexusXApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
